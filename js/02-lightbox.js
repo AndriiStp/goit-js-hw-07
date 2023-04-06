66564c82e52ef76536e1d9eba09ec1a6b08c2348
Andrii Stepanenko <andrii.stp@gmail.com>
2023-04-06 23:21:09 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+(function () {
+  const markup = galleryItems
+    .map(
+      ({ preview, description, original }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+    )
+    .join("");
+
+  list.insertAdjacentHTML("beforeend", markup);
+})();
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+lightbox.open();
